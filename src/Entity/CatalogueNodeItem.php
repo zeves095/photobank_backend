4cb311b3a5df1e040aa4cc9c90a410b3b95fd565
Unknown <dmitriy.efimov@yahoo.com>
2018-10-30 18:37:57 +0300

    Nullable nodeid

--- src/Entity/CatalogueNodeItem.php
+++ src/Entity/CatalogueNodeItem.php
@@ -25,7 +25,7 @@ class CatalogueNodeItem
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\CatalogueNode", inversedBy="items")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=true)
      */
     private $node;
 
