b50822f3a61d48b7f527cafbcd82302502f8816d
Unknown <dmitriy.efimov@yahoo.com>
2019-01-18 14:14:27 +0300

    JS tests start

---
+++ assets/js/tests/components/FormWrapper.spec.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {store} from '../../account/link-manager/store';
+import {selectors} from '../selectors';
+
+import {FormWrapper} from '../../forms/FormWrapper';
+
+Enzyme.configure({adapter: new Adapter()});
+
+let bareProps = {
+  onError:()=>{},
+  onChange:()=>{},
+  onSubmit:()=>{},
+  onError:()=>{},
+  onBlur:()=>{},
+  form:"",
+  isAdmin:false,
+  submit:()=>{},
+  defaults:{},
+  validate:()=>{},
+};
+
+let linkAddFormProps = {
+  defaults:{
+    resource:"5,7,9",
+    size:{},
+  },
+  dispatch: ()=>{},
+  form:"link-add",
+  isAdmin: ()=>{},
+  onBlur: ()=>{},
+  onChange: ()=>{},
+  onError: ()=>{},
+  onSubmit: ()=>{},
+  submit: ()=>{},
+  validate: ()=>{},
+}
+
+function setup(props){
+    const component = shallow(<FormWrapper {...props} />);
+
+    return{
+      props,
+      component
+    }
+}
+
+describe('FormWrapper', ()=>{
+  describe('render',()=>{
+    it('Компонент рендерится', ()=>{
+      const {component} = setup(bareProps);
+      expect(component.find(selectors.common.FORM_WRAPPER_WRAPPER).exists()).toBe(true);
+    });
+    it('По умолчанию дочерний компонент не рендерится', ()=>{
+      const {component} = setup(bareProps);
+      expect(component.find(selectors.components.FORM_SUBCOMPONENT).exists()).toBe(false);
+    });
+    it('Дочерний компонент отображается при указании формы', ()=>{
+      const {component} = setup(linkAddFormProps);
+      expect(component.find(selectors.components.FORM_SUBCOMPONENT).exists()).toBe(true);
+    });
+  });
+});
---
+++ assets/js/tests/components/LinkList.spec.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {store} from '../../account/link-manager/store';
+import {mockLinksDone} from '../mockdata/';
+import {selectors} from '../selectors';
+
+import {LinkList} from '../../account/link-manager/components/LinkList';
+import {FormWrapper} from '../../forms/FormWrapper';
+
+Enzyme.configure({adapter: new Adapter()});
+
+let bareProps = {
+  fetchLinks: ()=>{},
+  links: [],
+  chooseLink: ()=>{},
+  editing: false,
+  adding: false,
+  stopEditing: ()=>{},
+  addLink: ()=>{},
+  deleteLink: ()=>{},
+  thumbs: [],
+  targets: [],
+  loading: false,
+};
+
+let linksExistProps={
+  fetchLinks: ()=>{},
+  links: mockLinksDone,
+  chooseLink: ()=>{},
+  editing: false,
+  adding: false,
+  stopEditing: ()=>{},
+  addLink: ()=>{},
+  deleteLink: ()=>{},
+  thumbs: [],
+  targets: [],
+  loading: false,
+}
+
+function setup(props){
+    const component = shallow(<LinkList {...props} />);
+
+    return{
+      props,
+      component
+    }
+}
+
+describe('LinkList', ()=>{
+  describe('render',()=>{
+    it('Компонент рендерится', ()=>{
+      const {component} = setup(bareProps);
+      expect(component.find(selectors.linkmanager.LINK_LIST_WRAPPER).exists()).toBe(true);
+    });
+    it('Отображаются элементы', ()=>{
+      const {component} = setup(bareProps);
+      expect(component.find(selectors.COMPONENT_HEADER).exists()).toBe(true);
+      expect(component.find(selectors.COMPONENT_TITLE).exists()).toBe(true);
+      expect(component.find(selectors.COMPONENT_TOP_SECTION).exists()).toBe(true);
+      expect(component.find(selectors.BUTTON_BLOCK).exists()).toBe(true);
+      expect(component.find(selectors.HELP_TEXT).exists()).toBe(true);
+      expect(component.find(selectors.linkmanager.LINK_TARGETS).exists()).toBe(true);
+      expect(component.find(selectors.COMPONENT_HEADER).exists()).toBe(true);
+    });
+    it('Отображаются существующие ссылки', ()=>{
+      const {component} = setup(linksExistProps);
+      expect(component.find(selectors.linkmanager.LINK_LIST_ITEM).length).toBe(mockLinksDone.length);
+    });
+  });
+});
