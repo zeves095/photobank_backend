604935bfbffd3e7446600e37f10812c9a9cb603b
Dmitriy Efimov <dmitriy.efimov@yahoo.com>
2019-03-14 14:52:59 +0300

    fix(front/back): user access/styles fix

--- assets/js/photobank/components/CatalogueTree.jsx
+++ assets/js/photobank/components/CatalogueTree.jsx
@@ -206,7 +206,7 @@ export class CatalogueTree extends React.Component {
           <span className={"collection-tabs__tab" + (0==parseInt(this.props.collection_type,10)?" active":"")} onClick={()=>{this.handleCollectionChoice(0)}}>Товары</span>
         <span className={"collection-tabs__tab" + (1==parseInt(this.props.collection_type,10)?" active":"")} onClick={()=>{this.handleCollectionChoice(1)}}>Свалка</span>
         </div>
-        {this.props.isAuthorizedGarbage&&this.props.collection_type===1?<div><input type="checkbox" onChange={this.handleShowDeleted} defaultChecked={this.state.show_deleted} /><span className="lever" style={{marginLeft:"20px"}}>Показать удаленное</span></div>:null}
+        {this.props.isAuthorizedGarbage&&this.props.collection_type===1?<div onClick={this.handleShowDeleted} className="show-deleted-nodes"><input type="checkbox" onChange={this.handleShowDeleted} checked={this.state.show_deleted} /><span className="lever" style={{marginLeft:"20px"}}>Показать удаленное</span></div>:null}
       </span>
       <div className="inner-bump">
           <div className="catalogue-tree__crumbs crumbs">
