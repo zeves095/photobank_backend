639686a07ced4129e984f5be4e9e462cd5f44215
Dmitriy Efimov <dmitriy.efimov@yahoo.com>
2019-03-27 16:16:29 +0300

    feature(back): bulk get resource metadata

--- src/Controller/ExternalController.php
+++ src/Controller/ExternalController.php
@@ -18,6 +18,25 @@ class ExternalController extends AbstractController
 {
 
     /**
+     * Отпраляет метаданных о ресурсах товаров каталога и созданных для них пресетах
+     *
+     * @param String _codes коды товаров каталога
+     *
+     * @Route("/resources/getmetadata/bulk/{_codes}",
+     * methods={"GET"},
+     * name="resources_bulk_get_metadata")
+     */
+    public function getBulkResourceMeta($_codes, ResourceService $resourceService)
+    {
+      $response = new JsonResponse();
+
+      $codes = explode(',', $_codes);
+      $metadata = $resourceService->getItemResourcesMetadata($codes);
+      $response->setData($metadata);
+      return $response;
+    }
+
+    /**
      * Отпраляет метаданных о ресурсах товара каталога и созданных для них пресетах
      *
      * @param String _code код товара каталога
@@ -26,16 +45,16 @@ class ExternalController extends AbstractController
      * methods={"GET"},
      * name="resources_get_metadata")
      */
-    public function searchResources($_code, ResourceService $resourceService)
-    {
-      $response = new JsonResponse();
+     public function searchResources($_code, ResourceService $resourceService)
+     {
+       $response = new JsonResponse();
 
-      $metadata = $resourceService->getItemResourcesMetadata([$_code]);
-      if(!isset($metadata[$_code])||sizeof($metadata[$_code])<1){
-        throw new HttpException(404, "Не найдено ресурсов для товара");
-      }
-      $response->setData($metadata[$_code]);
-      return $response;
-    }
+       $metadata = $resourceService->getItemResourcesMetadata([$_code]);
+       if(!isset($metadata[$_code])||sizeof($metadata[$_code])<1){
+         throw new HttpException(404, "Не найдено ресурсов для товара");
+       }
+       $response->setData($metadata[$_code]);
+       return $response;
+     }
 
 }
